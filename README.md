333489d07e09bd7f30cb233800f45b8fe18901bf
Victor Jardim <54147711+vemcjardim@users.noreply.github.com>
2022-05-20 15:39:20 -0300

    Beta version

--- README.md
+++ README.md
@@ -1,7 +1,53 @@
 # Calculadora CLT
 
-The idea with this is to practice how to write javascript code with a goal to make a simple calculator.
 
-In Brazil, companies employees have their gross salary deduced by certain tax fees, like income tax and other fees. This simple script calculates the net salary and tells the fees that are deduced according to brazilian law.
+Live Demo: http://calculadoraclt.rf.gd/
 
-Click to see a [Live Demo] (http://calculadoraclt.rf.gd/)
+
+This labor calculator returns the net salary of a company emploee.
+
+In Brazil, to calcule companies employees net salary, first you need to calculate INSS contribution and IRRF fee, in a complex calculation.
+
+
+### INSS Contribution
+
+There are some ranges and rates
+
+Range 1 From 0       to 1212    => 7.5%
+Range 2 From 1212.01 to 2427.35 =>   9%
+Range 3 From 2427.36 to 3641.03 =>  12%
+Range 4 From 3641.04 to 7087.22 =>  14%
+
+If gross salary pass more than a range, you must apply rates for each range, like this:
+
+Lets say a emploee makes 3000 BRL per month:
+
+INSS:
+90.90  for Range 1 (1212 * 7.5%) +
+109.38 for Rnage 2 (2427.35 - 1212.01) * 9% +
+68.71  for Range 3 (3000 - 2427.36) * 12%
+Total 269
+
+
+### IRRF fee
+
+More ranges and rates... and deductions
+
+Range 1 From 0       to 1903.98  =>    0% => 0
+Range 2 From 1903.99 to 2826.65  =>  7.5% => 142.80
+Range 3 From 2826.66 to 3751.05  => 15.0% => 354.80
+Range 4 From 3751.06 to 4664.68  => 22.5% => 636.13
+Range 5 From 4664.68 to infinity => 27.5% => 869.36
+
+Same exemple of 3000 BRL per month:
+IRRF Base = 3000 - 269 (INSS) = 2731, so Range 2
+IRRF = (2731 * 7.5%) - 142.80 = 62.02
+
+
+### Net Salary
+
+Net Salary: 3000 - 269 (INSS) - 62.02 (IRRF) = 2668.97
+
+If emploee has other legal deductions on its payroll, like child support, it is deducted directly from gross salary.
+
+If emploee has child or legal dependents, 189.59 is deducted from IRRF base, per dependent.
--- calculadora.js
+++ calculadora.js
@@ -1,91 +1,115 @@
-function calcular() {
-
-    window.document.getElementById('data').style.display = 'table'
-            
-    // Get values from inputs
-    var salario = window.document.getElementById('salario')
-    var descontos = window.document.getElementById('descontos')
-    var dependentes = window.document.getElementById('dependentes')
-    
-    // Parse values to numbers
-    var salario_number = Number(salario.value)
-    var descontos_number = Number(descontos.value)
-    var dependentes_number = Number(dependentes.value)
-   
-    /*
-    ***
-    *** INSS rates
-    ***
-    RANGE                               RATE        DEDUCTION
-    Until R$ 1.212,00                    7,5%        90,90      First Range
-    From R$ 1.212,01 to R$ 2.427,35      9,0%       109.38      Second Range
-    From R$ 2.427,36 to R$ 3.641,03     12,0%       145.64      Third Range
-    From R$ 3.641,04 to R$ 7.087,22     14,0%       482.46      Fourth Range
-    */
-
-    // Compute INSS deduction
-    
-    if (salario_number <= 1212) {
-        // First Range
-        var inss = salario_number * 0.075
-        salario_number = salario_number.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        inss = inss.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        
-        window.document.getElementById('salbruto').innerHTML = salario_number
-        window.document.getElementById('inss').innerHTML = inss
-    } else if (salario_number <= 2427.35) {
-        // Second Range
-        var inss = ((salario_number - 1212) * 0.09) + 90.90
-        var inss_aliquota = (inss / salario_number)*100
-        inss = inss.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        window.document.getElementById('inss_aliquota').innerHTML = `${inss_aliquota.toFixed(2)} %`
-        window.document.getElementById('inss').innerHTML = inss
-    } else if (salario_number <= 3641.03) {
-        // Third Range
-        var inss = ((salario_number - 1212.01 - 1215.34) * 0.12) + 90.90 + 109.38
-        var inss_aliquota = (inss / salario_number)*100
-        inss = inss.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        window.document.getElementById('inss_aliquota').innerHTML = `${inss_aliquota.toFixed(2)} %`
-        window.document.getElementById('inss').innerHTML = inss
-    } else if (salario_number <= 7087.22) {
-        // Fourth Range
-        var inss = ((salario_number - 1212.01 - 1215.34 - 1213.68) * 0.14) + 90.90 + 109.38 + 145.64
-        var inss_aliquota = (inss / salario_number)*100
-        inss = inss.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        window.document.getElementById('inss_aliquota').innerHTML = `${inss_aliquota.toFixed(2)} %`
-        window.document.getElementById('inss').innerHTML = inss
+// Check if at least salary was input
+function check() {
+    let input_salary = window.document.getElementById('input_salary')
+    if (input_salary.value) {
+        compute()
     } else {
-        var inss = 828.39
-        var inss_aliquota = (inss / salario_number)*100
-        inss = inss.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
-        window.document.getElementById('inss_aliquota').innerHTML = `${inss_aliquota.toFixed(2)} % (TETO)`
-        window.document.getElementById('inss').innerHTML = inss
+        window.document.getElementById('error_msg').innerHTML = `<p>Digite ao menos o valor do salário</p>`
     }
-
-
-    /*
-    ***
-    *** IRPF rates and deduction per dependant
-    ***
-    Until R$ 1.903,98                    none         0,00
-    From R$ 1.903,99 to R$ 2.826,65      7,5%       142,80
-    From R$ 2.826,66 to R$ 3.751,05     15,0%       354,80
-    From R$ 3.751,06 to R$ 4.664,68     22,5%       636,13
-    After R$ 4.664,68                   27,5%       869,36
-    */
-
-
-    // Exibe o resultado
 }
 
-function limpar() {
-    window.document.getElementById('data').style.display = 'none'
-    window.document.getElementById('salario').value = ''
-    window.document.getElementById('descontos').value = ''
-    window.document.getElementById('dependentes').value = ''
-    window.document.getElementById('salbruto').innerHTML = '0,00'
-    window.document.getElementById('inss_aliquota').innerHTML = '0,00'
-    window.document.getElementById('inss').innerHTML = '0,00'
-    window.document.getElementById('irpf').innerHTML = '0,00'
+// Calculates net salary
+function compute() {
+    // Hides error message and shows table with results
+    window.document.getElementById('error_msg').style.display = 'none'
+    window.document.getElementById('show_table').style.display = 'table'
+    
+    // Gets values from inputs
+    let input_discount = window.document.getElementById('input_discount')
+    let input_dependent = window.document.getElementById('input_dependent')
 
+    // Turns inputs into numbers
+    input_salary = Number(input_salary.value)
+    input_discount = Number(input_discount.value)
+    input_dependent = Number(input_dependent.value)
+
+    // Makes ranges
+    let inss_range = [1212.00, 2427.35, 3641.03, 7087.22]
+    let irrf_range = [1903.98, 2826.65, 3751.05, 4664.68]
+
+    // Makes rates
+    let inss_rate = [7.5,  9.0, 12.0, 14.0]
+    let irrf_rate = [0, 7.5, 15.0, 22.5, 27.5]
+
+    // Makes deductions
+    let inss_deduction = [90.90,  109.38, 145.64, 482.46]
+    let irrf_deduction = [0, 142.80, 354.80, 636.13, 869.36]
+
+    // Makes dependent deduction
+    let dependent_value = 189.59
+    let dependent_deduction = input_dependent * dependent_value
+
+    // Finds where salary is in INSS range and calculates INSS contribution
+    if (input_salary <= inss_range[0]) {
+        inss_contribution = input_salary * inss_rate[0] / 100
+        inss_contribution = inss_contribution.toFixed(2)
+    } else if (input_salary <= inss_range[1]) {
+        inss_contribution = ((input_salary - (inss_range[0]+0.01)) * inss_rate[1] / 100) + inss_deduction[0]
+        inss_contribution = inss_contribution.toFixed(2)
+    } else if (input_salary <= inss_range[2]) {
+        inss_contribution = ((input_salary - (inss_range[1]+0.01)) * inss_rate[2] / 100) + inss_deduction[0] + inss_deduction[1]
+        inss_contribution = inss_contribution.toFixed(2)
+    } else if (input_salary <= inss_range[3]) {
+        inss_contribution = ((input_salary - (inss_range[2]+0.01)) * inss_rate[3] / 100) + inss_deduction[0] + inss_deduction[1] + inss_deduction[2]
+        inss_contribution = inss_contribution.toFixed(2)
+    } else {
+        inss_contribution = inss_deduction[0] + inss_deduction[1] + inss_deduction[2] + inss_deduction[3]
+        inss_contribution = inss_contribution.toFixed(2)
+    }
+
+    // Finds IRRF calculation basis
+    let irrf_base = input_salary - inss_contribution - dependent_deduction
+
+    // Finds where IRRF base is in IRRF range and calculates IRRF value
+    let irrf = 0
+    if (irrf_base <= irrf_range[0]) {
+        irrf = (irrf_base * irrf_rate[0] / 100) - irrf_deduction[0]
+    } else if (irrf_base <= irrf_range[1]) {
+        irrf = (irrf_base * irrf_rate[1] / 100) - irrf_deduction[1]
+    } else if (irrf_base <= irrf_range[2]) {
+        irrf = (irrf_base * irrf_rate[2] / 100) - irrf_deduction[2]
+    } else if (irrf_base <= irrf_range[3]) {
+        irrf = (irrf_base * irrf_rate[3] / 100) - irrf_deduction[3]
+    } else {
+        irrf = (irrf_base * irrf_rate[4] / 100) - irrf_deduction[4]
+    }
+
+    // Calculates Net Salary
+    let total_discounts = input_discount + Number(inss_contribution) + irrf
+    let net_salary = input_salary - inss_contribution - irrf - input_discount
+
+    // Calculates percents
+    let discounts_percent = (input_discount / input_salary * 100).toFixed(2)
+    let inss_percent = (inss_contribution / input_salary * 100).toFixed(2)
+    let irrf_percent = (irrf / input_salary * 100).toFixed(2)
+    let total_discounts_percent = (total_discounts / input_salary * 100).toFixed(2)
+    let net_salary_percent = (net_salary / input_salary * 100).toFixed(2)
+
+    // Shows results
+        // First line
+        window.document.getElementById('show_gross_salary_percent').innerHTML = `100 %`
+        window.document.getElementById('show_gross_salary').innerHTML = input_salary.toFixed(2)
+        // Second line
+        window.document.getElementById('show_discounts_percent').innerHTML = `${discounts_percent} %`
+        window.document.getElementById('show_discounts').innerHTML = input_discount.toFixed(2)
+        // Third line
+        window.document.getElementById('show_inss_percent').innerHTML = `${inss_percent} %`
+        window.document.getElementById('show_inss').innerHTML = inss_contribution
+        // 4th line
+        window.document.getElementById('show_irrf_percent').innerHTML = `${irrf_percent} %`
+        window.document.getElementById('show_irrf').innerHTML = irrf.toFixed(2)
+        // 5th line
+        window.document.getElementById('show_total_discounts_percent').innerHTML = `${total_discounts_percent} %`
+        window.document.getElementById('show_total_discounts').innerHTML = total_discounts.toFixed(2)
+        // 6th line
+        window.document.getElementById('show_net_salary_percent').innerHTML = `${net_salary_percent} %`
+        window.document.getElementById('show_net_salary').innerHTML = net_salary.toFixed(2)
+}
+
+// Clears all to start again
+function clear() {
+    window.document.getElementById('input_salary').value = ''
+    window.document.getElementById('input_discount').value = ''
+    window.document.getElementById('input_dependent').value = ''
+    window.document.getElementById('show_table').style.display = 'none'
 }
